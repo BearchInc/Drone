85e2b05d245e910487e743f469115aee6d746b36
Heck <heckfer@gmail.com>
2015-11-10 13:17:25 -0200

    Move app registration to HomeViewController.

--- DroneSpike/ProjectFiles/AppDelegate.swift
+++ DroneSpike/ProjectFiles/AppDelegate.swift
@@ -9,23 +9,23 @@
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate, DJIAppManagerDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        DJIAppManager.registerApp("602797533f7b529cc9470265", withDelegate: self)
+//        DJIAppManager.registerApp("602797533f7b529cc9470265", withDelegate: self)
         // Override point for customization after application launch.
         return true
     }
     
-    func appManagerDidRegisterWithError(errorCode: Int32) {
-		if errorCode == RegisterSuccess {
-			print("Registered successfully")
-		} else {
-			print("Error registering", errorCode)
-		}
-    }
+//    func appManagerDidRegisterWithError(errorCode: Int32) {
+//		if errorCode == RegisterSuccess {
+//			print("Registered successfully")
+//		} else {
+//			print("Error registering", errorCode)
+//		}
+//    }
 
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
--- DroneSpike/ViewControllers/HomeViewController.swift
+++ DroneSpike/ViewControllers/HomeViewController.swift
@@ -10,10 +10,46 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+	var drone: DJIDrone!
+	
+	@IBOutlet weak var debugLabel: UILabel!
+	
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+		
+		DJIAppManager.registerApp("602797533f7b529cc9470265", withDelegate: self)
     }
+	
+	func setDebugText(message: String) {
+		debugLabel.text = message
+	}
 
+}
+
+extension HomeViewController: DJIAppManagerDelegate, DJIDroneDelegate {
+	
+	func droneOnConnectionStatusChanged(status: DJIConnectionStatus) {
+		print("Status \(status)")
+	}
+	
+	func appManagerDidRegisterWithError(errorCode: Int32) {
+		if errorCode == RegisterSuccess {
+			setDebugText("Registered successfully")
+		} else {
+			print("Error registering", errorCode)
+		}
+	}
+	
+	func appManagerDidConnectedDroneChanged(newDrone: DJIDrone!) {
+		
+		var message = "New drone found"
+		if let drone = newDrone {
+			self.drone = drone
+		} else {
+			message = "error getting new drone"
+		}
+		
+		setDebugText(message)
+	}
+	
 }
