24a0abf99a275f8c0f64d16481af73085721752d
Heck <heckfer@gmail.com>
2015-11-10 12:53:32 -0200

    Fix app flow.

---
+++ DroneSpike/ViewControllers/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  DroneSpike
+//
+//  Created by Fernando Heck on 11/10/15.
+//  Copyright © 2015 Bearch Inc. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
