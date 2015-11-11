f8b7d336f4145ed644e2c8af23387f80ca21b40a
Lisardo/Ygor <lisardo.kist@gmail.com>
2015-11-11 10:52:56 -0200

    Added roll and pitch.

--- DroneSpike/ViewControllers/JoystickViewController.swift
+++ DroneSpike/ViewControllers/JoystickViewController.swift
@@ -3,9 +3,12 @@ import UIKit
 class JoystickViewController: UIViewController, DJIDroneDelegate {
     var throttle : Float = 0.0
     var yaw : Float = 0.0
+    var pitch : Float = 0.0
+    var roll : Float = 0.0
     lazy var drone = HomeViewController.drone
     
     @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         drone.delegate = self
         super.viewDidLoad()
@@ -15,9 +18,10 @@ class JoystickViewController: UIViewController, DJIDroneDelegate {
         drone.connectToDrone()
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        let value = UIInterfaceOrientation.LandscapeLeft.rawValue
+        UIDevice.currentDevice().setValue(value, forKey: "orientation")
     }
     
     func droneOnConnectionStatusChanged(status:DJIConnectionStatus) {
@@ -42,6 +46,27 @@ class JoystickViewController: UIViewController, DJIDroneDelegate {
         updateDrone()
     }
     
+    @IBAction func didTouchRollRight(sender: AnyObject) {
+        roll = 10
+        updateDrone()
+    }
+    
+    @IBAction func didTouchRollLeft(sender: AnyObject) {
+        roll = -10
+        updateDrone()
+    }
+    
+    @IBAction func didTouchPitchFoward(sender: AnyObject) {
+        pitch = 1
+        updateDrone()
+    }
+    
+    @IBAction func didTouchPitchBack(sender: AnyObject) {
+        pitch = -1
+        updateDrone()
+    }
+    
+    
     @IBAction func didTouchRight(sender: AnyObject) {
         yaw = 10
         updateDrone()
@@ -52,17 +77,17 @@ class JoystickViewController: UIViewController, DJIDroneDelegate {
         updateDrone()
     }
     
-    override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
-        return UIInterfaceOrientationMask.Landscape
-    }
-    
     func updateDrone() {
         var data = DJIFlightControlData()
         data.mThrottle = throttle
         data.mYaw = yaw
+        data.mPitch = pitch
+        data.mRoll = roll
         drone.mainController.navigationManager.flightControl.sendFlightControlData(data, withResult: nil)
         throttle = 0
         yaw = 0
+        pitch = 0
+        roll = 0
     }
     
     @IBAction func didTouchTakeOff(sender: AnyObject) {
@@ -74,6 +99,15 @@ class JoystickViewController: UIViewController, DJIDroneDelegate {
             }
         }
     
+    }
+    @IBAction func didTouchBackButton(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true) { () -> Void in
+            
+        }
+    }
+    
+    override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
+        return UIInterfaceOrientationMask.Landscape
     }
 }
 
