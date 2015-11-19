bc7ec1850ea72427d4600e45205ce4967a43589e
Ygor/Heck <ygor.bruxel@gmail.com>
2015-11-19 11:03:51 -0200

    Detect if the object is moving towards the camera

--- DroneSpike/ViewControllers/VideoMotionViewController.mm
+++ DroneSpike/ViewControllers/VideoMotionViewController.mm
@@ -112,12 +112,16 @@ using namespace std;
 
 - (NSArray *)meanShift:(NSArray *)frames {
     cv::Rect trackWindow;
+    cv::Rect previewsTrackWindow;
     
     NSMutableArray *resultImages = [NSMutableArray new];
     int hsize = 16;
     float hranges[] = {0,180};
     const float* phranges = hranges;
     Mat frame, hsv, hue, mask, hist, histimg = Mat::zeros(200, 320, CV_8UC3), backproj;
+    
+    Scalar redColor = Scalar(255, 0, 0);
+    Scalar greenColor = Scalar(0, 255, 0);
     
     for (UIImage *image in frames) {
         Mat imageMat = [ImageUtils cvMatFromUIImage:image];
@@ -127,62 +131,75 @@ using namespace std;
         cvtColor(frame, hsv, CV_BGR2HSV);
         
         
-//        if(??)
         
+        int _vmin = vmin, _vmax = vmax;
+        inRange(hsv, Scalar(0, smin, MIN(_vmin,_vmax)), Scalar(180, 256, MAX(_vmin, _vmax)), mask);
         
-            int _vmin = vmin, _vmax = vmax;
-            inRange(hsv, Scalar(0, smin, MIN(_vmin,_vmax)), Scalar(180, 256, MAX(_vmin, _vmax)), mask);
-            int ch[] = {0, 0};
-            hue.create(hsv.size(), hsv.depth());
-            mixChannels(&hsv, 1, &hue, 1, ch, 1);
+        Mat kernel = getStructuringElement( MORPH_RECT,
+                                           cv::Size( (2 * 1 + 1), (2 * 1 + 1)),
+                                           cv::Point( 1, 1 ) );
         
-            if (!hasHist) {
-                hasHist = true;
-                
-                Mat roi(hue, selection), maskroi(mask, selection);
-                
-                calcHist(&roi, 1, 0, maskroi, hist, 1, &hsize, &phranges);
-                normalize(hist, hist, 0, 255, NORM_MINMAX);
-                
-                trackWindow = selection;
-                
-                histimg = Scalar::all(0);
-                int binW = histimg.cols / hsize;
-                Mat buf(1, hsize, CV_8UC3);
-                for( int i = 0; i < hsize; i++ ) {
-                    buf.at<Vec3b>(i) = Vec3b(saturate_cast<uchar>(i*180./hsize), 255, 255);
-                }
-                cvtColor(buf, buf, COLOR_HSV2BGR);
-                
-                for( int i = 0; i < hsize; i++ ) {
-                    int val = saturate_cast<int>(hist.at<float>(i)*histimg.rows/255);
-                    
-                    cv::Point topLeft = cv::Point(i * binW, histimg.rows);
-                    cv::Point bottomRight = cv::Point((i + 1) * binW, histimg.rows - val);
-                    
-                    rectangle(histimg, topLeft, bottomRight, Scalar(buf.at<Vec3b>(i)), -1, 8 );
-                }
+        erode(hsv, hsv, kernel);
+        dilate(hsv, hsv, kernel);
+        
+//        mask = cv2.inRange(hsv, greenLower, greenUpper)
+//        mask = cv2.erode(mask, None, iterations=2)
+//        mask = cv2.dilate(mask, None, iterations=2)
+        
+        int ch[] = {0, 0};
+        hue.create(hsv.size(), hsv.depth());
+        mixChannels(&hsv, 1, &hue, 1, ch, 1);
+    
+        if (!hasHist) {
+            hasHist = true;
+            
+            Mat roi(hue, selection), maskroi(mask, selection);
+            
+            calcHist(&roi, 1, 0, maskroi, hist, 1, &hsize, &phranges);
+            normalize(hist, hist, 0, 255, NORM_MINMAX);
+            
+            trackWindow = selection;
+            previewsTrackWindow = selection;
+            
+            histimg = Scalar::all(0);
+            int binW = histimg.cols / hsize;
+            Mat buf(1, hsize, CV_8UC3);
+            for( int i = 0; i < hsize; i++ ) {
+                buf.at<Vec3b>(i) = Vec3b(saturate_cast<uchar>(i*180./hsize), 255, 255);
             }
-        
-            calcBackProject(&hue, 1, 0, hist, backproj, &phranges);
-            backproj &= mask;
-            trackBox = CamShift(backproj, trackWindow,
-                                TermCriteria( TermCriteria::EPS | TermCriteria::COUNT, 10, 1 ));
-        
-            if( trackWindow.area() <= 1 ) {
-                int cols = backproj.cols;
-                int rows = backproj.rows;
-                int r = (MIN(cols, rows) + 5)/6;
-                trackWindow = cv::Rect(trackWindow.x - r, trackWindow.y - r, trackWindow.x + r, trackWindow.y + r) &
-                cv::Rect(0, 0, cols, rows);
+            cvtColor(buf, buf, COLOR_HSV2BGR);
+            
+            for( int i = 0; i < hsize; i++ ) {
+                int val = saturate_cast<int>(hist.at<float>(i)*histimg.rows/255);
+                
+                cv::Point topLeft = cv::Point(i * binW, histimg.rows);
+                cv::Point bottomRight = cv::Point((i + 1) * binW, histimg.rows - val);
+                
+                rectangle(histimg, topLeft, bottomRight, Scalar(buf.at<Vec3b>(i)), -1, 8 );
             }
+        }
+    
+        calcBackProject(&hue, 1, 0, hist, backproj, &phranges);
+        backproj &= mask;
+        trackBox = CamShift(backproj, trackWindow,
+                            TermCriteria( TermCriteria::EPS | TermCriteria::COUNT, 10, 1 ));
+    
+        if( trackWindow.area() <= 1 ) {
+            int cols = backproj.cols;
+            int rows = backproj.rows;
+            int r = (MIN(cols, rows) + 5)/6;
+            trackWindow = cv::Rect(trackWindow.x - r, trackWindow.y - r, trackWindow.x + r, trackWindow.y + r) &
+            cv::Rect(0, 0, cols, rows);
+        }
+    
+        //    if( backprojMode )
+        //        cvtColor( backproj, image, COLOR_GRAY2BGR );
+        ellipse(imageMat, trackBox, Scalar(0,0,255));
         
-            //    if( backprojMode )
-            //        cvtColor( backproj, image, COLOR_GRAY2BGR );
-            ellipse(imageMat, trackBox, Scalar(0,0,255));
-            rectangle(imageMat, trackWindow.tl(), trackWindow.br(), Scalar(0, 0, 255));
-            [resultImages addObject:[ImageUtils UIImageFromCVMat:imageMat]];
-//    }
+        Scalar color = (previewsTrackWindow.area() * 1.1) < trackWindow.area() ? redColor : greenColor;
+        rectangle(imageMat, trackWindow.tl(), trackWindow.br(), color);
+        [resultImages addObject:[ImageUtils UIImageFromCVMat:imageMat]];
+        previewsTrackWindow = trackWindow;
     }
 
     return resultImages;
