e21d7ed957d268386bded41b77ca9f0dacc09c1b
Ygor <ygor.bruxel@gmail.com>
2015-11-20 15:20:33 -0200

    Extract camshift to another file

---
+++ DroneSpike/Opencv/CamshiftUtil.h
@@ -0,0 +1,15 @@
+//
+//  CamshiftUtil.h
+//  DroneSpike
+//
+//  Created by Ygor Bruxel on 11/20/15.
+//  Copyright © 2015 Bearch Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CamshiftUtil : NSObject
+
+- (NSArray *)meanShift:(NSArray *)frames;
+
+@end
