6c371c0a13148f020755feb095075c4e9b6c7909
Heck/Borges <heckfer@gmail.com>
2015-11-23 18:08:49 -0200

    Create another camshift method to test other stuff. WIP.

--- DroneSpike/Opencv/CamshiftUtil.mm
+++ DroneSpike/Opencv/CamshiftUtil.mm
@@ -28,7 +28,7 @@ using namespace cv;
     bool hasHist;
     int hsize;
     const float* phranges;
-    Mat hist, hue, mask;
+	Mat hist, hue, mask, hsv;
 }
 
 @end
@@ -51,18 +51,105 @@ using namespace cv;
         phranges = hranges;
         hue = Mat::zeros(180, 320, CV_8UC3);
         mask = Mat::zeros(180, 320, CV_8UC3);
+		
+
     }
     return self;
 }
 
-- (UIImage *)meanShift:(UIImage *)uiFrame {
-    Mat frame, hsv = Mat::zeros(180, 320, CV_8UC3), backproj;
+- (UIImage *)camshift:(UIImage *)uiFrame {
+	Mat imageMat = [ImageUtils cvMatFromUIImage:uiFrame];
+	Mat frame = Mat::zeros(180, 320, CV_8UC3), backproj;
+	hsv = Mat::zeros(180, 320, CV_8UC3);
+	cvtColor(imageMat, frame, CV_BGRA2BGR);
+	cvtColor(frame, hsv, CV_BGR2HSV);
+	
+	if (!hasHist) {
+		hasHist = YES;
+	
+		inRange(hsv, Scalar(0, smin, MIN(vmin, vmax)), Scalar(180, 256, MAX(vmin, vmax)), mask);
+		
+		int ch[] = {0, 0};
+		hue.create(hsv.size(), hsv.depth());
+		mixChannels(&hsv, 1, &hue, 1, ch, 1);
+		
+		Mat roi(hue, selection), maskroi(mask, selection);
+		
+		
+		
+		calcHist(&roi, 1, 0, maskroi, hist, 1, &hsize, &phranges);
+		normalize(hist, hist, 0, 255, NORM_MINMAX);
+		
+		trackWindow = selection;
+		previewsTrackWindow = selection;
+	}
+	
 
+	calcBackProject(&hue, 1, 0, hist, backproj, &phranges);
+	
+	backproj &= mask;
+	trackBox = CamShift(backproj, trackWindow, TermCriteria( TermCriteria::EPS | TermCriteria::COUNT, 10, 1 ));
+
+	Scalar color = (previewsTrackWindow.area() * 1.1) < trackWindow.area() ? redColor : greenColor;
+	previewsTrackWindow = trackWindow;
+	rectangle(imageMat, trackBox.boundingRect().tl(), trackBox.boundingRect().br(), color);
+	rectangle(imageMat, selection.tl(), selection.br(), Scalar(0, 0, 255), 2);
+	
+	return [ImageUtils UIImageFromCVMat:imageMat];
+}
+
+//6 # take first frame of the video
+//7 ret,frame = cap.read()
+//8
+//9 # setup initial location of window
+//10 r,h,c,w = 250,90,400,125  # simply hardcoded the values
+//11 track_window = (c,r,w,h)
+//12
+//13 # set up the ROI for tracking
+//14 roi = frame[r:r+h, c:c+w]
+//15 hsv_roi =  cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
+//16 mask = cv2.inRange(hsv_roi, np.array((0., 60.,32.)), np.array((180.,255.,255.)))
+//17 roi_hist = cv2.calcHist([hsv_roi],[0],mask,[180],[0,180])
+//18 cv2.normalize(roi_hist,roi_hist,0,255,cv2.NORM_MINMAX)
+//19
+//20 # Setup the termination criteria, either 10 iteration or move by atleast 1 pt
+//21 term_crit = ( cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1 )
+//22
+//23 while(1):
+//24     ret ,frame = cap.read()
+//25
+//26     if ret == True:
+//27         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+//28         dst = cv2.calcBackProject([hsv],[0],roi_hist,[0,180],1)
+//29
+//30         # apply meanshift to get the new location
+//31         ret, track_window = cv2.CamShift(dst, track_window, term_crit)
+//32
+//33         # Draw it on image
+//34         pts = cv2.boxPoints(ret)
+//35         pts = np.int0(pts)
+//36         img2 = cv2.polylines(frame,[pts],True, 255,2)
+//37         cv2.imshow('img2',img2)
+//38
+//39         k = cv2.waitKey(60) & 0xff
+//40         if k == 27:
+//41             break
+//42         else:
+//43             cv2.imwrite(chr(k)+".jpg",img2)
+//44
+//45     else:
+//46         break
+//47
+//48 cv2.destroyAllWindows()
+//49 cap.release()
+
+- (UIImage *)meanShift:(UIImage *)uiFrame {
+	Mat imageMat = [ImageUtils cvMatFromUIImage:uiFrame];
+	Mat frame, hsv = Mat::zeros(180, 320, CV_8UC3), backproj;
+	
 	NSLog(@"Image wxh: %f x %f", uiFrame.size.width, uiFrame.size.height);
-    Mat imageMat = [ImageUtils cvMatFromUIImage:uiFrame];
 	
     cvtColor(imageMat, frame, CV_BGRA2BGR);
-	
     cvtColor(frame, hsv, CV_BGR2HSV);
 	
     inRange(hsv, Scalar(0, smin, MIN(vmin, vmax)), Scalar(180, 256, MAX(vmin, vmax)), mask);
@@ -82,8 +169,8 @@ using namespace cv;
         hasHist = true;
         
         Mat roi(hue, selection), maskroi(mask, selection);
-        Mat histimg = Mat::zeros(200, 320, CV_8UC3);
-        
+		
+
         calcHist(&roi, 1, 0, maskroi, hist, 1, &hsize, &phranges);
         normalize(hist, hist, 0, 255, NORM_MINMAX);
         
@@ -92,7 +179,8 @@ using namespace cv;
         
         
         previewsTrackWindow = selection;
-        
+
+		Mat histimg = Mat::zeros(200, 320, CV_8UC3);
         histimg = Scalar::all(0);
         int binW = histimg.cols / hsize;
         Mat buf(1, hsize, CV_8UC3);
@@ -115,8 +203,7 @@ using namespace cv;
 	
     calcBackProject(&hue, 1, 0, hist, backproj, &phranges);
     backproj &= mask;
-    trackBox = CamShift(backproj, trackWindow,
-                        TermCriteria( TermCriteria::EPS | TermCriteria::COUNT, 10, 1 ));
+    trackBox = CamShift(backproj, trackWindow, TermCriteria( TermCriteria::EPS | TermCriteria::COUNT, 10, 1 ));
     
     if( trackWindow.area() <= 1 ) {
         int cols = backproj.cols;
@@ -125,7 +212,7 @@ using namespace cv;
         trackWindow = cv::Rect(trackWindow.x - r, trackWindow.y - r, trackWindow.x + r, trackWindow.y + r) &
         cv::Rect(0, 0, cols, rows);
     }
-    
+	
     //    if( backprojMode )
     //        cvtColor( backproj, image, COLOR_GRAY2BGR );
     Scalar color = (previewsTrackWindow.area() * 1.1) < trackWindow.area() ? redColor : greenColor;
